616077e54cadd2b8995dc9cc97226edc3b51ff3c
Jake <jtschwantes@gmail.com>
2019-11-16 17:44:15 -0700

    finalizing setup

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const conversions = require('./rates.js');
+
+const server = express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .use(bodyParser.urlencoded({ extended: true }))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .post('/getRate', (req, res) => {
+        const {weight, type} = req.body;
+        let price = conversions[type][weight];
+        let name = conversions[type].name;
+        if (price !== undefined) return res.render('pages/rate.ejs', {weight, name, price});
+        else res.render('pages/error.ejs', {weight, name})
+    });
+
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
