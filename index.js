1ebe602858b7563cf34f37ef0fecff2afd6d7c73
Jake <jtschwantes@gmail.com>
2019-11-16 18:28:15 -0700

    finished test project

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ const PORT = process.env.PORT || 5000;
 
 const conversions = require('./rates.js');
 
+function calculate(req, res) {
+    const {weight, type} = req.body;
+    let price = conversions[type][weight];
+    let name = conversions[type].name;
+    if (price !== undefined) return res.render('pages/rate.ejs', {weight, name, price});
+    else res.render('pages/error.ejs', {weight, name})
+}
+
 const server = express()
     .use(express.static(path.join(__dirname, 'public')))
     .use(bodyParser.urlencoded({ extended: true }))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
-    .post('/getRate', (req, res) => {
-        const {weight, type} = req.body;
-        let price = conversions[type][weight];
-        let name = conversions[type].name;
-        if (price !== undefined) return res.render('pages/rate.ejs', {weight, name, price});
-        else res.render('pages/error.ejs', {weight, name})
-    });
+    .post('/getRate', calculate);
 
 server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
