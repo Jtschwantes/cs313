8e62f938e2560e5f187d41ccaf46810c0807b79c
Jake <jtschwantes@gmail.com>
2019-12-16 16:45:07 -0700

    testing db

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ const server = express()
             // const results = { 'results': (result) ? result.rows : null };
             let results = result.rows;
             console.log(results)
+            console.log(JSON.stringify(results));
             results = JSON.parse(results);
             // const item = results.where(i => i.id == id);
             res.send(item);
